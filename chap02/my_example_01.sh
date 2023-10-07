1fc7d60bbe7511ac8dc88ef940aab246338d1a2c
Lauri Kyttälä <lauri.kyttala@gmail.com>
2023-10-07 17:56:59 +0300

    Initial commit

---
+++ chap02/my_example_01.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# Example page 43 Ellie Quigley
+
+variable_name='command'
+echo $variable_name
+
+now=`date`
+echo $now
+echo "Today is `date`"
+
+
+n=`expr 5 + 5`
+echo $n
+
+
---
+++ chap02/my_multiple_args.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# printing multiple arguments
+
+echo "Please give me your first and lastname: "
+read name
+read lastname
+echo "Nimesi on: " $name $lastname
+
+
